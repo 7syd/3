5e19dc1611b4d89b1338686cdcdb82295819600a
notStudent69 <134788809+notstudent69@users.noreply.github.com>
2023-11-07 08:29:39 +0530

    Update connec.py

--- connec.py
+++ connec.py
@@ -3,21 +3,25 @@ import mysql.connector
 new_db = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "root",
-    database = "test1"
+    password = "6969",
+    database = "tester"
 )
 
 new_cursor = new_db.cursor()
 
-table_query = "Create table tester1(id INT, name VARCHAR(200), age INT)"
+table_query = "Create table tester6(id INT, name VARCHAR(200), age INT)"
 new_cursor.execute(table_query)
 
-insert_query = "Insert into tester1(id,name,age) Values (%s,%s,%s)"
+new_db.commit()
+
+insert_query = "Insert into tester6(id,name,age) Values (%s,%s,%s)"
 vals = [('101','ash','20'),('102','sidd','20'),('103','harsh','19')]
 
 new_cursor.executemany(insert_query,vals)
 
-display_query = "Select * From tester1"
+new_db.commit()
+
+display_query = "Select * From tester6"
 
 new_cursor.execute(display_query)
 new_data = new_cursor.fetchall()
@@ -26,8 +30,12 @@ print("\nInserted values Table : \n")
 for x in new_data:
     print(x)
 
-update_query = "Update tester1 set age = '20' where id = '103'"
+
+
+update_query = "Update tester6 set age = '20' where id = '103'"
 new_cursor.execute(update_query)
+
+new_db.commit()
 
 new_cursor.execute(display_query)
 new_data2 = new_cursor.fetchall()
@@ -38,8 +46,11 @@ for x in new_data2:
 
 
 
-delete_query = "Delete from tester1 where id = '103'"
+
+delete_query = "Delete from tester6 where id = '103'"
 new_cursor.execute(delete_query)
+
+new_db.commit()
 
 new_cursor.execute(display_query)
 new_data3 = new_cursor.fetchall()
@@ -47,3 +58,4 @@ new_data3 = new_cursor.fetchall()
 print("\nDeleted values Table : \n")
 for x in new_data3:
     print(x)
+
